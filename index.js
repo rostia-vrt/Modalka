3e1bf5add49eda99e968ccb72c2eb509c51c5c75
rostia-vrt <vrostyk07@gmail.com>
2020-02-21 17:55:37 +0200

    made some changes

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const modal = $.modal();
+const fruits = [
+    {id: 1, title: 'Apple', price: 20},
+    {id: 2, title: 'Orange', price: 30},
+    {id: 3, title: 'Mango', price: 40}
+];
+
+const modal = $.modal({
+    title: 'My Modal',
+    closable: true,
+    content: `
+    <p>Lorem ipsum dolor sit.</p>
+    <p>Lorem ipsum dolor sit.</p>
+    `,
+    width: '400px',
+    footerButtons: [
+        {text: 'ok', type: 'primary', handler() {
+            console.log('Primary btn clicked');  
+            modal.close();
+        }},
+        {text: 'Cancel', type: 'danger', handler() {
+            console.log('Danger btn clicked');  
+            modal.close();
+        }}
+    ]
+});
